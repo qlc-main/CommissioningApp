b198d43c1d28de1e62d804cf7fbb609850a009c0
Raphael Williams <rwilliams@qlcnfs.quadlogic.com>
2019-10-07 15:19:07 -0400

    Readded function that was accidentally removed

--- WpfCommApp/Views/ConnectView.xaml.cs
+++ WpfCommApp/Views/ConnectView.xaml.cs
@@ -12,5 +12,22 @@ namespace WpfCommApp
         {
             InitializeComponent();
         }
+
+        private void coms_click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            button.ContextMenu.IsOpen = !button.ContextMenu.IsOpen;
+
+            if (button.ContextMenu.IsOpen)
+            {
+                button.ContextMenu.PlacementTarget = button;
+                button.ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+                button.ContextMenu.IsEnabled = true;
+            }
+            else
+            {
+                button.ContextMenu.IsEnabled = false;
+            }
+        }
     }
 }
